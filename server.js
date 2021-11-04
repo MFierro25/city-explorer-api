e18f330fa138f17bea432614b96270309f8e4ffc
Miguel Fierro <emfierro2@gmail.com>
2021-11-04 10:11:57 -0700

    made working weather api backend

--- server.js
+++ server.js
@@ -12,22 +12,23 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001
 
-
+app.get('/hello', (req, res) => res.status(200).send('Hello'));
 app.get('/weather', handleGetWeather);
+// app.get('/movies', handleGetMovies);
 
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
-function getForecastData(city) {
-    return city.data.map((day) => {
-        const date = day.datetime;
-        const description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
-        return new WeatherForecast(description, date);
-    })
-}
+// function getForecastData(city) {
+//     return city.data.map((day) => {
+//         const date = day.datetime;
+//         const description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
+//         return new WeatherForecast(description, date);
+//     })
+// }
 
 class WeatherForecast {
     constructor(obj) {
-        this.descripton = obj.description
+        this.descripton = obj.weather.description
         this.date = obj.date
     }
 }
@@ -39,23 +40,22 @@ async function handleGetWeather (req, res) {
 
     const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`;
     
-        const weatherResults = await axios.get(url)
-            .then((res) => {
-                return res.data;
-            })
-            .catch((error) => {
-                if (error.response) {
-                    console.log(error);
-                    throw new Error('Invalid search');
-                }
-            });
+        const results = await axios.get(url)
+        const forecastData = results.data.data.map(forecast => new WeatherForecast(forecast))
+           res.status(200).send(forecastData);
+//             .catch((error) => {
+//                 if (error.response) {
+//                     console.log(error);
+//                     throw new Error('Invalid search');
+//                 }
+//             });
 
-    if (weatherResults) {
-        const forecastData = getForecastData(weatherResults);
-        res.send(forecastData);
-    } else {
-        throw new Error ('Invalid'); 
+//     if (weatherResults) {
+//         const forecastData = getForecastData(weatherResults);
+//         res.send(forecastData);
+//     } else {
+//         throw new Error ('Invalid'); 
+// }
 }
-};
         
 app.listen(PORT, () => console.log(`server is up on ${PORT}`));
